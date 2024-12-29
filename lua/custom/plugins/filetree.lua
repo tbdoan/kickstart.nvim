a0965a6c9f1703e4aac4834c0fc20fb50349ab7a
tdoan <tdoan@anduril.com>
2024-12-28 17:21:11 -0800

    clean bindings for nvim tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -8,6 +8,9 @@ return {
           quit_on_open = true,
         },
       },
+      renderer = {
+        group_empty = true,
+      },
       on_attach = function(bufnr)
         local api = require 'nvim-tree.api'
 
@@ -15,12 +18,36 @@ return {
           return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
         end
 
-        -- default mappings
-        api.config.mappings.default_on_attach(bufnr)
+        -- BEGIN_DEFAULT_ON_ATTACH
+        vim.keymap.set('n', 'K', api.node.show_info_popup, opts 'Info')
+        vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts 'Open: Vertical Split')
+        vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts 'Open: Horizontal Split')
+        vim.keymap.set('n', '<CR>', api.node.open.edit, opts 'Open')
+        vim.keymap.set('n', '<Tab>', api.node.open.preview, opts 'Open Preview')
+        vim.keymap.set('n', 'a', api.fs.create, opts 'Create File Or Directory')
+        vim.keymap.set('n', 'bd', api.marks.bulk.delete, opts 'Bookmark: Delete')
+        vim.keymap.set('n', 'bt', api.marks.bulk.trash, opts 'Bookmark: Trash')
+        vim.keymap.set('n', 'bmv', api.marks.bulk.move, opts 'Bookmark: Move')
+        vim.keymap.set('n', 'c', api.fs.copy.node, opts 'Copy')
+        vim.keymap.set('n', 'D', api.fs.remove, opts 'Delete')
+        vim.keymap.set('n', 'd', api.fs.trash, opts 'Trash')
+        vim.keymap.set('n', 'E', api.tree.expand_all, opts 'Expand All')
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+        vim.keymap.set('n', 'm', api.marks.toggle, opts 'Bookmark: Toggle')
+        vim.keymap.set('n', 'p', api.fs.paste, opts 'Paste')
+        vim.keymap.set('n', 'P', api.node.navigate.parent, opts 'Parent Directory')
+        vim.keymap.set('n', 'r', api.fs.rename, opts 'Rename')
+        vim.keymap.set('n', '<c-r>', api.tree.reload, opts 'Refresh')
         vim.keymap.set('n', 'M', api.tree.collapse_all, opts 'Collapse All')
-        vim.keymap.del('n', '<C-e>', { buffer = bufnr })
-        local treeutils = require 'custom.utils.treeutils'
+        vim.keymap.set('n', 'x', api.fs.cut, opts 'Cut')
+        vim.keymap.set('n', 'yw', api.fs.copy.filename, opts 'Copy Name')
+        vim.keymap.set('n', 'yy', api.fs.copy.relative_path, opts 'Copy Relative Path')
+        vim.keymap.set('n', 'yY', api.fs.copy.absolute_path, opts 'Copy Absolute Path')
+        vim.keymap.set('n', 'f.', api.tree.toggle_hidden_filter, opts 'Toggle Filter: Dotfiles')
+        vim.keymap.set('n', 'fi', api.tree.toggle_gitignore_filter, opts 'Toggle Filter: Git Ignore')
+        -- END_DEFAULT_ON_ATTACH
 
+        local treeutils = require 'custom.utils.treeutils'
         vim.keymap.set('n', '<leader>fif', treeutils.launch_find_files, opts 'Launch Find Files')
         vim.keymap.set('n', '<leader>fig', treeutils.launch_live_grep, opts 'Launch Live Grep')
       end,
